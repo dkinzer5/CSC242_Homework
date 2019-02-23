fa814036f5761fa537dd7d191675bd46f5dbd1bf
dkinzer5 <d.kinzer5@gmail.com>
2019-02-23 12:31:33 -0800

    Finished second excercise

--- Week3/HW_5_27.cpp
+++ Week3/HW_5_27.cpp
@@ -9,34 +9,48 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
+
 using namespace std;
+
+//Print a readable header at the top 
 void printHeader()
 {
-    cout << setw(15)<< left << "i value" << "PI value" << endl;
+	cout << setw(18) << left << "i value" << "PI value" << endl;
 }
+
+//Print a value table that shows what pi equals 
+//when using the series formula at different i values
+void printPiTable()
+{
+	// Declare variables to sum the series 
+	//and another to increment the value of i by 10,000
+	double sum = 0.0;
+	int increment = 10000;
+
+	while (increment <= 100000)
+	{
+		for (int i = 1; i < increment; i++)
+		{
+			int exponent = i + 1;
+			double numerator = pow(-1, exponent);
+			double denominator = 2 * i - 1;
+			double myNumber = numerator / denominator;
+			sum += myNumber;
+		}
+
+		double PI = 4 * sum;
+		cout << setprecision(6) << fixed;
+		cout << setw(8) << left << increment << "--------" << "> " << PI << endl;
+		increment += 10000;
+		sum = 0;
+	}
+}
+
 int main()
 {
-    double sum = 0.0;
-    int increment = 10000;
-    
-    printHeader();
-    while(increment <= 100000)
-    {
-        for (int i = 1; i<increment; i++)
-        {
-            int exponent = i + 1;
-            double numerator = pow(-1, exponent);
-            double denominator = 2*i - 1;
-            double myNumber = numerator / denominator;
-            sum += myNumber;
-        }
-        
-        double PI = 4*sum;
-        increment += 10000;
-        sum = 0;
-    }
-   
-    
-    
-    return 0;
+	
+	printHeader();
+	printPiTable();
+
+	return 0;
 }

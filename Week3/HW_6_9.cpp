89daed2dcd851bf7222ffba0dd3639498b7fec11
dkinzer5 <d.kinzer5@gmail.com>
2019-02-25 12:49:33 -0800

    more changes to specificity

--- Week3/HW_6_9.cpp
+++ Week3/HW_6_9.cpp
@@ -2,26 +2,40 @@
 #include <iomanip>
 using namespace std;
 
+// Convert celsius to farenheit
 double celsiustoFarenheit(double celsius)
 {
 	double farenheit = (9.0 / 5) * celsius + 32;
 	return farenheit;
 }
 
+// Convert farenheit to celsius
 double farenheitToCelsius(double farenheit)
 {
 	double celsius = (5.0 / 9) * (farenheit - 32);
 	return celsius;
 }
 
-int main()
+void printHeader()
 {
-	cout << fixed << setprecision(2);
 	cout << setw(12) << left << "Celsius" << setw(15) << left << "Farenheit" << setw(6) << left << "|" << setw(15) << left << "Farenheit" << setw(12) << left << "Celsius" << endl;
+}
+
+void printTable()
+{
 	for (int c = 40, f = 120; c > 30; c--, f -= 10)
 	{
 		cout << setw(12) << left << c << setw(15) << left << celsiustoFarenheit(c) << setw(6) << left << "|" << setw(15) << left << f << setw(12) << left << farenheitToCelsius(f) << endl;
 	}
+}
+int main()
+{
+	// Format floating point numbers with 2 decimals
+	cout << fixed << setprecision(2);
+
+	//Print header with table below
+	printHeader();
+	printTable();
 
 	return 0;
 }
